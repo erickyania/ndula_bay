1d98b94a9090ab5b5e74440b2402d136130a598d
eric kyania <erickyania0@gmail.com>
2023-11-21 10:18:18 +0300

    The ndulabay project

---
+++ ndulabay/src/database/index.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const configOptions = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+const connectToDB = async () => {
+  const connectionUrl =
+    "mongodb+srv://ericmulwa71:oRCYxoJnQg5ftM1Z@cluster0.pwl4bpc.mongodb.net/";
+
+  mongoose
+    .connect(connectionUrl, configOptions)
+    .then(() => console.log("Ecommerce database connected successfully!"))
+    .catch((err) =>
+      console.log(`Getting Error from DB connection ${err.message}`)
+    );
+};
+
+export default connectToDB;
